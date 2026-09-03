6e7daf67fdb276af2c6ef7a3ba1d1794351f76ce
cole <ch4iiky@gmail.com>
2026-09-03 14:25:19 +0200

    Added finished login screen with register and forgot password functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,14 @@ import LectureDashboard from './pages/LectureDashboard';
 import LandingPage from './pages/LandingPage';
 import './index.css';
 import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <LandingPage/>
+    //<LandingPage/>
+    //<LectureDashboard/>
+   // <Dashboard/>
+   <Login/>
 
   );
 }
