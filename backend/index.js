62aa41d4c2109567a29abca47b930e11e2b2ad57
ernst <ernst.n.641@gmail.com>
2026-09-04 20:25:04 +0200

    add test doc, add doc fetch api call

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import pool from './db.js';
+import SupportingDocument from './models/SupportingDocument.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -39,6 +40,38 @@ app.get('/api/metrics/tables', async (req, res) => {
     res.json({ tables });
   } catch (err) {
     res.status(500).json({status: 'error', error: err.message})
+  }
+});
+
+app.get('/api/fetch/doc/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const dbRes = await pool.query(
+      `SELECT "DocumentID", "StudentID", "DocumentType", "FilePath",
+              "UploadTimestamp"
+       FROM "SUPPORTING_DOCUMENT"
+       WHERE "DocumentID" = $1`,
+      [id]
+    );
+
+    if (dbRes.rows.length === 0) {
+      return res.status(404).json({
+        status: 'error',
+        error: 'Document not found',
+      });
+    }
+
+    const document = SupportingDocument.fromDb(dbRes.rows[0]);
+    const fileBuffer = await document.getFile();
+
+    res.type(document.file_path);
+    res.send(fileBuffer);
+  } catch (err) {
+    res.status(500).json({
+      status: 'error',
+      error: err.message,
+    });
   }
 });
 
