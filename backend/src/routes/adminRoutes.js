12ea612ea371c0587d50492ad7da2c402834ebae
ernst <ernst.n.641@gmail.com>
2026-09-14 18:38:46 +0200

    Update authMiddleware to include role authorization, update admin dashboard fecth to validate role

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import AdminController from '../controllers/AdminController.js';
-import {authenticate} from '../middlewares/authMiddleware.js'
+import {authenticate, authorize} from '../middlewares/authMiddleware.js'
 
 const router = express.Router();
 
-<<<<<<< HEAD
 router.get(
     '/dashboard_statistics', 
     AdminController.getDashboardSummary
 );
-=======
+
 router.get('/dashboard_statistics', authenticate, AdminController.getDashboardSummary);
->>>>>>> df401c0 (Add authentication to admin dashboard fetch request)
+
+router.get('/dashboard_statistics', authenticate, authorize([3]), AdminController.getDashboardSummary);
 
 export default router;
