405631fa907a03db12a6ba6bb74cde004f3c28ce
ernst <ernst.n.641@gmail.com>
2026-09-14 17:56:40 +0200

    Add authentication to admin dashboard fetch request

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import AdminController from '../controllers/AdminController.js';
+import {authenticate} from '../middlewares/authMiddleware.js'
 
 const router = express.Router();
 
+<<<<<<< HEAD
 router.get(
     '/dashboard_statistics', 
     AdminController.getDashboardSummary
 );
+=======
+router.get('/dashboard_statistics', authenticate, AdminController.getDashboardSummary);
+>>>>>>> df401c0 (Add authentication to admin dashboard fetch request)
 
 export default router;
