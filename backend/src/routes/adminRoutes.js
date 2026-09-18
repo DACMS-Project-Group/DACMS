d23145e3ee8db429ece57622a5fc69ec6bcf6c5b
nwbester <31783430@mynwu.ac.za>
2026-09-18 12:36:58 +0200

    Neil local build with final tweaks and fixes

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -4,13 +4,6 @@ import {authenticate, authorize} from '../middlewares/authMiddleware.js'
 
 const router = express.Router();
 
-router.get(
-    '/dashboard_statistics', 
-    AdminController.getDashboardSummary
-);
-
-router.get('/dashboard_statistics', authenticate, AdminController.getDashboardSummary);
-
 router.get('/dashboard_statistics', authenticate, authorize([3]), AdminController.getDashboardSummary);
 
 export default router;
