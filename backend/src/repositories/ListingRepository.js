edafb3e86d28095a9947d316ed6ddfe8d7e59f8d
nwbester <31783430@mynwu.ac.za>
2026-09-20 22:06:41 +0200

    Added listing repo

---
+++ backend/src/repositories/ListingRepository.js
@@ -0,0 +1,52 @@
+import pool from '../config/db.js'
+
+class ListingRepository { 
+    static async getOpenListings() {
+        const rows = await this.query(
+            `
+            SELECT "ListingID", "ModuleID", "Deadline"
+            FROM "DEMI_LISTING"
+            WHERE "Deadline" > NOW()
+            ORDER BY "Deadline" ASC
+            `
+        )
+        return rows 
+    }
+
+    static async getListingById(listingId) {
+        const rows = await this.query(
+            `
+            SELECT "ListingID", "ModuleID", "Deadline"
+            FROM "DEMI_LISTING"
+            WHERE "ListingID" = $1
+            `,
+            [listingId]
+        )
+        return rows[0] || null
+    }
+
+    static async createListing( {moduleId, lecturerId, deadline, minimumGrade} ) {
+        const rows = await this.query(
+            `
+            INSERT INTO "DEMI_LISTING" ("ModuleID", "LecturerID", "Deadline", "MinimumGrade")
+            VALUES ($1, $2, $3, $4)
+            RETURNING *
+            `,
+            [moduleId, lecturerId, deadline, minimumGrade]
+        )
+        return rows[0] || null
+    }
+
+    static async editListing(listingId, {moduleId, lecturerId, deadline, minimumGrade}) {
+        const rows = await this.query(
+            `
+            UPDATE "DEMI_LISTING"
+            IF SET "ModuleID" = $1, "LecturerID" = $2, "Deadline" = $3, "MinimumGrade" = $4
+            WHERE "ListingID" = $5
+            RETURNING *
+            `,
+            [moduleId, lecturerId, deadline, minimumGrade, listingId]
+        )
+        return rows[0] || null
+    }
+}
