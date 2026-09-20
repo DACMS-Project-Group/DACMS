b53f926042e3c0537a372631f91bebb100e59d37
nwbester <31783430@mynwu.ac.za>
2026-09-20 22:25:09 +0200

    Implemented listings under lecturer

--- backend/src/routes/lecturerRoutes.js
+++ backend/src/routes/lecturerRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import LecturerController from '../controllers/LecturerController.js';
+import ListingController from '../controllers/ListingController.js';
 
 import {
     authenticate,
@@ -16,4 +17,31 @@ router.get(
     LecturerController.getDashboardSummary
 );
 
+router.get(
+    '/listings/open',
+    authenticate,
+    authorize([2]),
+    ListingController.getOpenListings
+);
+
+router.get(
+    '/listings/:Id',
+    authenticate,
+    authorize([2]),
+    ListingController.getListingById
+);
+
+router.post(
+    '/listings/create',
+    authenticate,
+    authorize([2]),
+    ListingController.createListing
+);
+
+router.patch(
+    '/listings/edit/:Id',
+    authenticate,
+    authorize([2]),
+    ListingController.editListing
+);
 export default router;
